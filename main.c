53c89f9abfdc39c402dfa6b3562e2773cf7b5aac
srathina <mailtorsaravan@gmail.com>
2021-10-16 10:40:44 +0530

    include library and include directories commands

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include "name.h"
 
 int main(void)
 {
-    printf("hello world");
+    name();
 }
---
+++ name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "name.h"
+
+void name(void)
+{
+    printf("RSK\n");
+    printf("SKA\n");
+    printf("Tanish\n");
+    printf("Task\n");
+}
